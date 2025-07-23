c2017876cdca35bd9b00099a48c3928412e9a1db
deypadma <padma@klizos.com>
2025-07-23 19:45:34 +0530

    add AsyncClient from httpx

--- tests/test_userdata.py
+++ tests/test_userdata.py
@@ -1,8 +1,28 @@
-import pytest
-from fastapi.testclient import TestClient
-from backend.main import app
+# import pytest
+# from fastapi.testclient import TestClient
+# from backend.main import app
 
-test_client = TestClient(app)
+# test_client = TestClient(app)
+
+# @pytest.mark.asyncio
+# async def test_userdata():
+#     payload = {
+#         "name": "test1",
+#         "email": "test1@klizos.com",
+#         "phone_no": 7063315080,
+#         "password": "Klizos@123"
+#     }
+#     response = test_client.post("/userdata", json=payload)
+#     assert response.status_code == 200
+
+#     data = response.json()
+#     assert data['msg'] == "User data entered"
+#     assert data['status'] is True
+
+
+import pytest
+from httpx import AsyncClient
+from backend.main import app
 
 @pytest.mark.asyncio
 async def test_userdata():
@@ -12,9 +32,9 @@ async def test_userdata():
         "phone_no": 7063315080,
         "password": "Klizos@123"
     }
-    response = test_client.post("/userdata", json=payload)
+    async with AsyncClient(app=app, base_url="http://test") as ac:
+        response = await ac.post("/userdata", json=payload)
     assert response.status_code == 200
-
     data = response.json()
     assert data['msg'] == "User data entered"
     assert data['status'] is True
